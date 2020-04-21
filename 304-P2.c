9b7d9f961874c6b147427b26b55c08213ca39a74
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-04-21 11:43:11 +0300

    time function and command parsing added

--- 304-P2.c
+++ 304-P2.c
@@ -3,14 +3,29 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <unistd.h>
+#include <sys/time.h>
+
+#define MATCH(s) (!strcmp(argv[ac], (s)))
 
 static int plane_id = 0;
+
+static const double kMicro = 1.0e-6;
+double get_time() {
+	struct timeval TV;
+	struct timezone TZ;
+	const int RC = gettimeofday(&TV, &TZ);
+	if(RC == -1) {
+		printf("ERROR: Bad call to gettimeofday\n");
+		return(-1);
+	}
+	return( ((double)TV.tv_sec) + kMicro * ((double)TV.tv_usec) );
+}
 
 typedef struct Plane {
     int ID;
     int arrival_time;
-    pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
 }Plane;
 
 typedef struct Queue {
@@ -32,7 +47,7 @@ Plane* createPlane(int arrival_time){
     pthread_cond_init(&plane->cond, NULL);
 
     return plane;
-}
+};
 
 Queue* createQueue(int max_planes)
 {
@@ -45,13 +60,13 @@ Queue* createQueue(int max_planes)
     queue->planes = (Plane*) malloc(max_planes * sizeof(Plane));
 
     return queue;
-}
+};
 
 void enqueue(Queue *queue, Plane plane)
 {
     if(queue->size == queue->capacity)
     {
-            printf("Queue is Full\n");
+            printf("Queue is full\n");
     }
     else
     {
@@ -61,15 +76,15 @@ void enqueue(Queue *queue, Plane plane)
             {
                     queue->rear = 0;
             }
-            queue->elements[queue->rear] = plane;
+            queue->planes[queue->rear] = plane;
     }
-}
+};
 
 void dequeue(struct Queue *queue)
 {
-    if(queue->size==0)
+    if(queue->size == 0)
     {
-        printf("Queue is Empty\n");
+        printf("Queue is empty\n");
     }
     else
     {
@@ -77,7 +92,47 @@ void dequeue(struct Queue *queue)
         queue->front++;
         if(queue->front == queue->capacity)
         {
-            queue->front=0;
+            queue->front = 0;
         }
     }
+};
+
+void landing()
+{
+
+};
+
+void departing()
+{
+
+};
+
+void air_control()
+{
+
+};
+
+
+
+int main(int argc, char* argv[])
+{
+    float prob = 0.5;
+    int simulation_time = 100;
+    
+    if(argc<2) {
+	  printf("Usage: %s [-i < filename>] [-iter <n_iter>] [-l <lambda>] [-o <outputfilename>]\n",argv[0]);
+	  return(-1);
+	}
+	for(int ac=1;ac<argc;ac++) {
+		if(MATCH("-p")) {
+			prob = atof(argv[++ac]);
+		} else if(MATCH("-s")) {
+			simulation_time = atoi(argv[++ac]);
+		} else {
+		    printf("Usage: %s [-s <simulation time>] [-p <probability>]\n",argv[0]);
+		    return(-1);
+		}
+	}
+
+
 }
